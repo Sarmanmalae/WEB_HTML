5a119b85645758a33bd2ce076fe651b0aaa32095
Sarmanmalae <nikitin230905@gmail.com>
2022-03-12 08:50:50 +0300

    Carousel commit

--- Рекламная кампания.py
+++ Рекламная кампания.py
@@ -391,49 +391,53 @@ def sample_file_upload():
 
 @app.route('/carousel')
 def ex_last():
-    return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                             <link rel="stylesheet"
-                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                             crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пейзажы Новой Зеландии</title>
-                          </head>
-                          <body>
-                            <h1 align="center" ">Пейзажы Новой Зеландии</h1>
-                            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                              <div class="carousel-inner">
-                                <div align="center" class="carousel-item active" data-bs-interval="1">
-                                  <img src="{url_for('static', filename='img/pic1.jpg')}" class="d-block w-50" alt="здесь должна была быть картинка, но не нашлась">
-                                </div>
-                                <div align="center" class="carousel-item" data-bs-interval="1">
-                                  <img src="{url_for('static', filename='img/pic2.jpg')}" class="d-block w-50" alt="здесь должна была быть картинка, но не нашлась">
-                                </div>
-                                <div align="center" class="carousel-item" data-bs-interval="1">
-                                  <img src="{url_for('static', filename='img/pic3.jpg')}" class="d-block w-50" alt="здесь должна была быть картинка, но не нашлась">
-                                </div>
-                                <div align="center" class="carousel-item" data-bs-interval="1">
-                                  <img src="{url_for('static', filename='img/pic4.jpg')}" class="d-block w-50" alt="здесь должна была быть картинка, но не нашлась">
-                                </div>
-                                <div align="center" class="carousel-item" data-bs-interval="1">
-                                  <img src="{url_for('static', filename='img/pic5.jpg')}" class="d-block w-50" alt="здесь должна была быть картинка, но не нашлась">
-                                </div>
-                              </div>
-                              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                              </button>
-                              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                              </button>
-                            </div>
-                          </body>
-                        </html>'''
+    return f'''<html lang="en">
+                    <head>
+                      <title>Пейзажи Марса</title>
+                      <meta charset="utf-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1">
+                      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                    </head>
+                    <body>
+                    
+                    <div class="container">
+                      <h1 align="center">Пейзажи Марса</h1>  
+                      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
+                    
+                        <div class="carousel-inner">
+                          <div class="item active">
+                            <img src="/static/img/pic1.jpg" alt="..." style="width:100%; height:100%">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="/static/img/pic2.jpg" alt="..." style="width:100%; height:100%">
+                          </div>
+                        
+                          <div class="item">
+                            <img src="/static/img/pic3.jpg" alt="..." style="width:100%; height:100%">
+                          </div>
+                        </div>
+                    
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>
+                    </div>
+                    
+                    </body>
+                </html>'''
 
 
 if __name__ == '__main__':
